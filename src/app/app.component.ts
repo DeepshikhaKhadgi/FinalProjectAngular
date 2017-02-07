a2bb5b3814e1e9c8067b393dfd1f97f224c42bf0
Deepshikha Khadgi <deepshikhakhadgi@gmail.com>
2017-02-07 15:51:44 -0600

    added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private db: DataService) {
+    this.db.getMyData().subscribe(res => console.log(res));
+  }
+
+
+
 }
