2fd49c04c74fc38e7f6250674b77309c44a357bc
Deepshikha Khadgi <deepshikhakhadgi@gmail.com>
2017-02-09 12:34:44 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-import {DataService} from './data.service';
+
+import {DataService} from './services/data.service';
+
+import {MyRoutesModule} from './app.routes';
+
+import { StatesComponent } from './components/states/states.component';
+import { CitiesComponent } from './components/cities/cities.component';
+import { ItemsComponent } from './components/items/items.component';
+import { ItemsNearMeComponent } from './components/items-near-me/items-near-me.component';
+import { SearchedItemsComponent } from './components/searched-items/searched-items.component';
+import {ToolbarComponent} from './components/toolbar.component';
+import {AuthService} from './services/auth.service';
+import { DonateComponent } from './components/donate/donate.component';
+import {AddItemCanActivate} from './components/donate/donate.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatesComponent,
+    CitiesComponent,
+    ItemsComponent,
+    ItemsNearMeComponent,
+    SearchedItemsComponent,
+    ToolbarComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MyRoutesModule,
+    ReactiveFormsModule
   ],
-  providers: [DataService],
+  providers: [DataService, AuthService, AddItemCanActivate],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
